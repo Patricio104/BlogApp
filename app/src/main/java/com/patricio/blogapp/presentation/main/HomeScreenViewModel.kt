16135f8f60041a0f0bdba546aaec456ba2729960
Patricio104 <patriciojuarez104@gmail.com>
2021-06-22 12:07:26 -0500

    Se agrega flow como otra forma de obtener los datos

--- app/src/main/java/com/patricio/blogapp/presentation/main/HomeScreenViewModel.kt
+++ app/src/main/java/com/patricio/blogapp/presentation/main/HomeScreenViewModel.kt
@@ -3,9 +3,15 @@ package com.patricio.blogapp.presentation.main
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
 import com.patricio.blogapp.core.Result
+import com.patricio.blogapp.data.model.Post
 import com.patricio.blogapp.domain.home.HomeScreenRepo
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.stateIn
 
 class HomeScreenViewModel(private val repo: HomeScreenRepo): ViewModel() {
     fun fetchLatestPosts() = liveData(Dispatchers.IO) {
@@ -16,6 +22,20 @@ class HomeScreenViewModel(private val repo: HomeScreenRepo): ViewModel() {
             emit(Result.Failure(e))
         }
     }
+
+    val latestPost : StateFlow<Result<List<Post>>> = flow {
+        kotlin.runCatching {
+            repo.getLatestPost()
+        }.onSuccess { resultPostList ->
+            emit(resultPostList)
+        }.onFailure { trowable ->
+            emit(Result.Failure(Exception(trowable)))
+        }
+    }.stateIn(
+        scope = viewModelScope,
+        started = SharingStarted.WhileSubscribed(5000),
+        initialValue = Result.Loading()
+    )
 }
 
 class HomeScreenViewModelFactory(private val repo: HomeScreenRepo): ViewModelProvider.Factory{
--- app/src/main/java/com/patricio/blogapp/ui/home/HomeScreenFragment.kt
+++ app/src/main/java/com/patricio/blogapp/ui/home/HomeScreenFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import com.patricio.blogapp.R
 import com.patricio.blogapp.core.Result
 import com.patricio.blogapp.core.hide
@@ -16,6 +19,8 @@ import com.patricio.blogapp.domain.home.HomeScreenRepoImpl
 import com.patricio.blogapp.presentation.main.HomeScreenViewModel
 import com.patricio.blogapp.presentation.main.HomeScreenViewModelFactory
 import com.patricio.blogapp.ui.home.adapter.HomeScreenAdapter
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 
 class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
@@ -30,6 +35,37 @@ class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeScreenBinding.bind(view)
 
+        //Usando Stateflow
+
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED){
+                viewModel.latestPost.collect { result ->
+                    when (result) {
+                        is Result.Loading -> {
+                            binding.progressBar.show()
+                        }
+
+                        is Result.Success -> {
+                            binding.progressBar.hide()
+                            if (result.data.isEmpty()){
+                                binding.emptyContainer.show()
+                                return@collect
+                            }
+                            binding.rvHome.adapter = HomeScreenAdapter(result.data)
+                        }
+
+                        is Result.Failure -> {
+                            binding.progressBar.hide()
+                            Toast.makeText(requireContext(), "Ocurrió un error: ${result.exception}", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+            }
+        }
+
+
+        //USando livedata observer
+        /*
         viewModel.fetchLatestPosts().observe(viewLifecycleOwner, Observer { result ->
             when (result) {
                 is Result.Loading -> {
@@ -50,7 +86,7 @@ class HomeScreenFragment : Fragment(R.layout.fragment_home_screen) {
                     Toast.makeText(requireContext(), "Ocurrió un error: ${result.exception}", Toast.LENGTH_SHORT).show()
                 }
             }
-        })
+        })*/
     }
 
 }
